9b85687566396ab9c2c159f59f704f2a9e9a8e6b
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-12-07 02:43:03 +0900

    :memo: fix: 기능변경을 제거로 인한 path 값을 제거한다.

--- src/app/(postView)/diary/page.tsx
+++
@@ -1,7 +0,0 @@
-import POLICY from '@/constants/policy'
-import DomainHeaderSection from '@/app/(postView)/_components/common/domain-header-section'
-
-export default function Page() {
-  const { domain, description } = POLICY.domainInfo.diary
-  return <DomainHeaderSection domain={domain} description={description} />
-}
--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,20 +2,12 @@ export type PathType = keyof typeof PATH
 export type HeaderPath = Exclude<PathType, 'login' | 'signup'>
 export type SocialType = 'google' | 'kakao' | 'naver'
 
-export const HEADER_PATH: HeaderPath[] = [
-  'home',
-  'posts',
-  'projects',
-  'issues',
-  'diary',
-] as const
+export const HEADER_PATH: HeaderPath[] = ['home', 'posts', 'projects'] as const
 
 export const PATH = {
   home: '/',
   posts: '/posts',
   projects: '/projects',
-  issues: '/issues',
-  diary: '/diary',
   login: '/login',
   signup: '/signup',
 } as const
